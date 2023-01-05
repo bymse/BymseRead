97fbb599046ecbea3baeb301c96f2680b4781447
bymse <egorstrelov@gmail.com>
2023-01-06 02:51:56 +0300

    Back button

---
+++ BymseBooks.Ui/Models/ITopBarItem.cs
@@ -0,0 +1,19 @@
+﻿namespace BymseBooks.Ui.Models;
+
+public interface ITopBarItem
+{
+    ItemType Type { get; }
+}
+
+public enum ItemType
+{
+    Link,
+}
+
+public class LinkTopBarItem : ITopBarItem
+{
+    public ItemType Type => ItemType.Link;
+    public string Text { get; init; }
+    public string Url { get; init; }
+    public string Css { get; init; }
+}
