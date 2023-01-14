28b814fb060eebc3b4c23bda93338e0875b53ea5
bymse <egorstrelov@gmail.com>
2023-01-15 00:33:55 +0300

    BookForm setup

---
+++ BymseBooks.Core/Models/BookFormModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BymseBooks.Core.Models;
+
+public class BookFormModel
+{
+    public string Title { get; set; }
+    public string? Author { get; set; }
+    public string[] Tags { get; set; }
+    public string? Url { get; set; }
+}
