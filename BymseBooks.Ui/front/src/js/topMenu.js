015bb811e37b5b568728fdd0913e0a3155164d12
bymse <egorstrelov@gmail.com>
2023-03-13 00:35:11 +0300

    TopMenu

---
+++ BymseBooks.Ui/front/src/js/topMenu.js
@@ -0,0 +1,8 @@
+﻿
+window.handleOutsideClick = (dotnetHelper) => {
+  window.addEventListener('click', e => {
+    if (!e.target.closest('[data-top-menu]')) {
+      dotnetHelper.invokeMethodAsync("OnOutsideClick")
+    }
+  })
+}
