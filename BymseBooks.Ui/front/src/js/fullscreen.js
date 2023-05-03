c3ad38334fcf26d0f86adf4fff37e91129020994
bymse <egorstrelov@gmail.com>
2023-05-04 00:23:25 +0300

    Fullscreen draft

---
+++ BymseBooks.Ui/front/src/js/fullscreen.js
@@ -0,0 +1,9 @@
+﻿
+
+window.openFullscreen = async (selector) => {
+  await document.querySelector(selector).requestFullscreen();
+}
+
+window.exitFullscreen = async () => {
+  await document.exitFullscreen();
+}
