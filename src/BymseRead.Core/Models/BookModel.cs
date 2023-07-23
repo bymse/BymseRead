bbddd887c2d1ad597c2eb28f58abf5ed015e5f39
bymse <egorstrelov@gmail.com>
2023-07-23 14:44:31 +0300

    New book form fix

--- src/BymseRead.Core/Models/BookModel.cs
+++ src/BymseRead.Core/Models/BookModel.cs
@@ -5,13 +5,14 @@ namespace BymseRead.Core.Models;
 public class BookModel
 {
     public int Id { get; init; }
-    public string Title { get; set; }
-    public string[] Tags { get; set; }
-    public string Author { get; set; }
+    public string? Title { get; set; }
+    public string[] Tags { get; set; } = Array.Empty<string>();
+    public string? Author { get; set; }
     public BookState State { get; set; }
     public int? TotalPages { get; set; }
     public int? LastViewedPage { get; init; }
     public string? Url { get; set; }
     public int? PercentageRead { get; init; }
+    
     public string[] TagsWithHashes => Tags.Select(e => $"#{e}").ToArray();
 }
