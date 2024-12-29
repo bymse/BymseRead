050e35fe672538b2f70c85ad21ba3377e79c1cb4
bymse <egorstrelov@gmail.com>
2024-12-29 20:32:25 +0300

    Nuke local setup

---
+++ build/Targets/SetupLocal.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+using System.Text.Json;
+using Nuke.Common;
+using Nuke.Common.IO;
+
+partial class Build
+{
+    Target SetupDevConfig => target => target
+        .Executes(() =>
+        {
+            var paths = new[]
+            {
+                Solution.BymseRead_Service.Directory / "appsettings.Development.json",
+                Solution.BymseRead_DbMigrator.Directory / "appsettings.Development.json"
+            };
+            
+            var config = new
+            {
+                ConnectionStrings = new
+                {
+                    DefaultConnection = $"Host=localhost;Port={LocalPostgresPort};" +
+                                        $"Database=BymseRead;Username={LocalPostgresUser};" +
+                                        $"Password={LocalPostgresPassword}"
+                }
+            };
+
+            var json = JsonSerializer.Serialize(config, new JsonSerializerOptions
+            {
+                WriteIndented = true,
+                IndentSize = 2,
+            });
+
+            foreach (var path in paths.Where(e => !e.FileExists()))
+            {
+                path.WriteAllText(json);
+            }
+        });
+
+    Target SetupLocal => target => target
+        .DependsOn(UpDatabase, ApplyMigrations, SetupDevConfig)
+        .Executes(() =>
+        {
+            Serilog.Log.Information("Setup completed");
+        });
+}
