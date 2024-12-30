e40ce36304c4c2d4a32781f4862798fb3b83804b
bymse <egorstrelov@gmail.com>
2024-12-30 11:11:43 +0300

    Indexes added

---
+++ src/BymseRead.DbMigrator/Migrations/AddIndexes.cs
@@ -0,0 +1,27 @@
+﻿using BymseRead.Infrastructure.Database;
+using FluentMigrator;
+
+namespace BymseRead.DbMigrations.Migrations;
+
+[Migration(2)]
+public class AddIndexes : Migration
+{
+    public override void Up()
+    {
+        Create
+            .Index()
+            .OnTable(Tables.Books)
+            .OnColumn("owner_user_id")
+            .Ascending();
+
+        Create
+            .Index()
+            .OnTable(Tables.Bookmarks)
+            .OnColumn("user_id").Ascending()
+            .OnColumn("book_id").Ascending()
+            .OnColumn("created_at").Descending()
+            ;
+    }
+
+    public override void Down() {}
+}
