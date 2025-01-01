e4bfe3cdf64e58fcadca0f4dc5c7af11c9e80e25
bymse <egorstrelov@gmail.com>
2025-01-01 14:42:06 +0300

    Nuke client generator

--- build/Build.cs
+++ build/Build.cs
@@ -6,10 +6,12 @@ using Nuke.Common.Execution;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
+using Nuke.Common.Tools.DotNet;
 using Nuke.Common.Utilities.Collections;
 using static Nuke.Common.EnvironmentInfo;
 using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.IO.PathConstruction;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
 partial class Build : NukeBuild
 {
@@ -19,7 +21,7 @@ partial class Build : NukeBuild
     ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
     ///   - Microsoft VSCode           https://nuke.build/vscode
 
-    public static int Main () => Execute<Build>(x => x.Compile);
+    public static int Main () => Execute<Build>(x => x.CompileService);
 
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
@@ -27,21 +29,13 @@ partial class Build : NukeBuild
     [Solution(GenerateProjects = true)]
     readonly Solution Solution = null!;
 
-    Target Clean => _ => _
-        .Before(Restore)
+    Target CompileService => target => target
         .Executes(() =>
         {
-        });
-
-    Target Restore => _ => _
-        .Executes(() =>
-        {
-        });
-
-    Target Compile => _ => _
-        .DependsOn(Restore)
-        .Executes(() =>
-        {
+            DotNetBuild(e => e
+                .SetProjectFile(Solution.BymseRead_Service)
+                .SetConfiguration(Configuration)
+            );
         });
 
 }
---
+++ build/Targets/ClientGenerator.cs
@@ -0,0 +1,35 @@
+﻿using Nuke.Common;
+using Nuke.Common.IO;
+using Nuke.Common.Tooling;
+
+partial class Build
+{
+    AbsolutePath OpenApiDoc => Solution.BymseRead_Service_Client.Directory / "openapi.json";
+
+    [NuGetPackage(packageId: "Swashbuckle.AspNetCore.Cli",
+        packageExecutable: "dotnet-swagger.dll",
+        Framework = "net8.0")]
+    Tool SwashbuckleCli;
+
+    [NuGetPackage(
+        packageId: "Microsoft.OpenApi.Kiota",
+        packageExecutable: "kiota.dll",
+        Framework = "net8.0")]
+    Tool Kiota;
+
+    Target GenerateDoc => target => target
+        .DependsOn(CompileService)
+        .Executes(() =>
+        {
+            var dllPath = Solution.BymseRead_Service.Directory / "bin" / Configuration / "net8.0" / "BymseRead.Service.dll";
+            SwashbuckleCli($"tofile --output {OpenApiDoc} {dllPath} WebApi");
+        });
+
+    Target GenerateClient => target => target
+        .DependsOn(GenerateDoc)
+        .Executes(() =>
+        {
+            Kiota(
+                $"generate -d {OpenApiDoc} -l CSharp -c BymseReadClient -n BymseRead.Service.Client -o {Solution.BymseRead_Service_Client.Directory / "Client"}");
+        });
+}
