6b6eaf4e9101d35dca8b3677615a7c91b53e86e4
bymse <egorstrelov@gmail.com>
2025-01-01 13:05:19 +0300

    Books and files repository

---
+++ src/BymseRead.Infrastructure/Database/Repositories/BooksRepository.cs
@@ -0,0 +1,34 @@
+﻿using BymseRead.Core.Common;
+using BymseRead.Core.Entities;
+using BymseRead.Core.Repositories;
+using Dapper;
+
+namespace BymseRead.Infrastructure.Database.Repositories;
+
+[AutoRegistration]
+internal class BooksRepository(ConnectionFactory connectionFactory) : IBooksRepository
+{
+    public async Task Add(Book book)
+    {
+        var connection = await connectionFactory.Get();
+        const string sql = """
+                            insert into books (
+                                               id,
+                                               title,
+                                               tags,
+                                               created_at,
+                                               pages,
+                                               book_file_id,
+                                               book_cover_file_id,
+                                               owner_user_id
+                                               )
+                           values (@Id, @Title, @Tags, @CreatedAt, @Pages, @BookFileId, @BookCoverFileId, @OwnerUserId);
+                           """;
+
+        var insertedRows = await connection.ExecuteAsync(sql, book);
+        if (insertedRows != 1)
+        {
+            throw new InvalidOperationException("Failed to insert book");
+        }
+    }
+}
---
+++ src/BymseRead.Infrastructure/Database/Repositories/FilesRepository.cs
@@ -0,0 +1,25 @@
+﻿using BymseRead.Core.Common;
+using BymseRead.Core.Repositories;
+using Dapper;
+using File = BymseRead.Core.Entities.File;
+
+namespace BymseRead.Infrastructure.Database.Repositories;
+
+[AutoRegistration]
+internal class FilesRepository(ConnectionFactory connectionFactory) : IFilesRepository
+{
+    public async Task Add(File file)
+    {
+        var connection = await connectionFactory.Get();
+        const string sql = """
+                            insert into files (id, name, path, "size", created_at)
+                            values (@Id, @Name, @Path, @Size, @CreatedAt);
+                           """;
+
+        var insertedRows = await connection.ExecuteAsync(sql, file);
+        if (insertedRows != 1)
+        {
+            throw new InvalidOperationException("Failed to insert file");
+        }
+    }
+}
