f84fbc935f658052900e7d5bba315ceb52a2f6cc
bymse <egorstrelov@gmail.com>
2025-01-03 19:25:59 +0300

    Remove finished at column

--- src/BymseRead.Core/Entities/BookProgress.cs
+++ src/BymseRead.Core/Entities/BookProgress.cs
@@ -8,7 +8,6 @@ public class BookProgress
     public UserId UserId { get; init; }
     
     public DateTimeOffset StartedAt { get; init; } = DateTimeOffset.UtcNow;
-    public DateTimeOffset? FinishedAt { get; set; }
     public DateTimeOffset? CurrentPageChangeAt { get; init; }
     
     public int CurrentPage { get; init; }
---
+++ src/BymseRead.DbMigrator/Migrations/RemoveFinishedAt.cs
@@ -0,0 +1,17 @@
+﻿using FluentMigrator;
+
+namespace BymseRead.DbMigrations.Migrations;
+
+[Migration(5)]
+public class RemoveFinishedAt : Migration
+{
+    public override void Up()
+    {
+        Delete.Column("finished_at").FromTable("books");
+    }
+
+    public override void Down()
+    {
+        
+    }
+}
