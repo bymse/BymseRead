3d7556d162a2144473270357aad4d641af4db178
bymse <egorstrelov@gmail.com>
2025-01-06 12:48:57 +0300

    Header setup

---
+++ src/WebClientApp/src/components/Header/Header.stories.tsx
@@ -0,0 +1,14 @@
+﻿import { Header } from './Header'
+import { Meta, StoryObj } from '@storybook/preact'
+
+const meta: Meta<typeof Header> = {
+  component: Header,
+  args: {},
+}
+export default meta
+
+type Story = StoryObj<typeof Header>
+
+export const Default: Story = {
+  args: {},
+}
---
+++ src/WebClientApp/src/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+﻿import styles from './Header.module.css'
+
+export const Header = () => {
+  return (
+    <header className={styles.header}>
+      <h1>Header</h1>
+    </header>
+  )
+}
