43acfb258ffc5b03e8b93c16e8b24dff1315d471
bymse <egorstrelov@gmail.com>
2025-01-09 21:28:04 +0300

    Search input

---
+++ src/WebClientApp/src/components/Input/Input.stories.tsx
@@ -0,0 +1,16 @@
+﻿import { Input, InputProps } from './Input'
+import { Meta, StoryObj } from '@storybook/preact'
+
+const meta: Meta<InputProps> = {
+  component: Input,
+}
+export default meta
+
+type Story = StoryObj<InputProps>
+
+export const Search: Story = {
+  args: {
+    type: 'search',
+    placeholder: 'Placeholder',
+  },
+}
---
+++ src/WebClientApp/src/components/Input/Input.tsx
@@ -0,0 +1,20 @@
+﻿import styles from './Input.module.css'
+import { SearchIcon } from '@icons/SearchIcon.tsx'
+
+export type InputProps = {
+  type: 'search'
+  placeholder?: string
+}
+
+export const Input = ({ type, placeholder }: InputProps) => {
+  return (
+    <div className={styles.wrapper}>
+      {type === 'search' && (
+        <div className={styles.icon}>
+          <SearchIcon />
+        </div>
+      )}
+      <input type={type} className={styles.input} placeholder={placeholder} />
+    </div>
+  )
+}
