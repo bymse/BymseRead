4fef4d03e2a30e5be0ec8067d7c8e1a67f6905e3
bymse <egorstrelov@gmail.com>
2025-01-10 08:57:49 +0300

    Dropdown

---
+++ src/WebClientApp/src/components/Dropdown/Dropdown.stories.tsx
@@ -0,0 +1,41 @@
+﻿import { Dropdown, DropdownItem, DropdownProps } from './Dropdown'
+import { Meta, StoryObj } from '@storybook/preact'
+import { Button } from '@components/Button/Button.tsx'
+import { MoreHorIcon } from '@icons/MoreHorIcon.tsx'
+
+const meta: Meta<DropdownProps> = {
+  component: Dropdown,
+  args: {},
+}
+export default meta
+
+type Story = StoryObj<DropdownProps>
+
+export const Left: Story = {
+  args: {
+    side: 'left',
+    button: ({ onClick }) => <Button appearance="flat" icon={MoreHorIcon} onClick={onClick} />,
+    children: (
+      <>
+        <DropdownItem>text</DropdownItem>
+        <DropdownItem>text</DropdownItem>
+      </>
+    ),
+  },
+}
+
+export const Right: Story = {
+  args: {
+    side: 'right',
+    button: ({ onClick }) => <Button appearance="flat" icon={MoreHorIcon} onClick={onClick} />,
+    children: (
+      <>
+        <DropdownItem>text</DropdownItem>
+        <DropdownItem>text</DropdownItem>
+      </>
+    ),
+  },
+  parameters: {
+    layout: 'centered',
+  },
+}
---
+++ src/WebClientApp/src/components/Dropdown/Dropdown.tsx
@@ -0,0 +1,49 @@
+﻿import { ComponentChildren, FunctionalComponent } from 'preact'
+import styles from './Dropdown.module.css'
+import { useEffect, useRef, useState } from 'preact/hooks'
+import cn from 'classnames'
+
+export interface DropdownButtonProps {
+  onClick: () => void
+}
+
+export type DropdownProps = {
+  children: ComponentChildren
+  button: FunctionalComponent<DropdownButtonProps>
+  side: 'right' | 'left'
+}
+
+export const Dropdown: FunctionalComponent<DropdownProps> = ({ children, button, side }: DropdownProps) => {
+  const [isOpen, setIsOpen] = useState(false)
+  const dropdownRef = useRef<HTMLDivElement>(null)
+
+  const handleClickOutside = (event: MouseEvent) => {
+    if (isOpen && dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+      setIsOpen(false)
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside)
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+  }, [])
+
+  const handleToggle = () => setIsOpen(r => !r)
+  const Button = button
+  return (
+    <div className={styles.dropdown} ref={dropdownRef}>
+      <Button onClick={handleToggle} />
+      {isOpen && <ul className={cn(styles.list, styles[side])}>{children}</ul>}
+    </div>
+  )
+}
+
+type DropdownItemProps = {
+  children: ComponentChildren
+}
+
+export const DropdownItem: FunctionalComponent<DropdownItemProps> = ({ children }: DropdownItemProps) => {
+  return <li className={styles.item}>{children}</li>
+}
