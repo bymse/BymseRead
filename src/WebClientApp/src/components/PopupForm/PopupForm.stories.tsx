3ea316ce1828c040603510883497e495bc54a14f
bymse <egorstrelov@gmail.com>
2025-01-28 22:05:51 +0300

    Popup form

---
+++ src/WebClientApp/src/components/PopupForm/PopupForm.stories.tsx
@@ -0,0 +1,29 @@
+﻿import { PopupFormProps, PopupForm } from './PopupForm.tsx'
+import { Meta, StoryObj } from '@storybook/preact'
+import { fn } from '@storybook/test'
+import { Input } from '@components/Input/Input.tsx'
+
+const meta: Meta<PopupFormProps> = {
+  component: PopupForm,
+  args: {},
+}
+export default meta
+
+type Story = StoryObj<PopupFormProps>
+
+export const Default: Story = {
+  args: {
+    title: 'Title',
+    onSubmit: fn(),
+    onCancel: fn(),
+    submitTitle: 'Submit',
+    children: (
+      <>
+        <Input type="search" />
+      </>
+    ),
+  },
+  parameters: {
+    layout: 'centered',
+  },
+}
---
+++ src/WebClientApp/src/components/PopupForm/PopupForm.tsx
@@ -0,0 +1,41 @@
+﻿import styles from './PopupForm.module.css'
+import { ComponentChildren } from 'preact'
+import { Button } from '@components/Button/Button.tsx'
+import { CloseIcon } from '@icons/CloseIcon.tsx'
+
+export type PopupFormProps = {
+  title: string
+  onCancel: () => void
+  onSubmit: () => void
+  children: ComponentChildren
+  submitTitle: string
+}
+
+export const PopupForm = ({ title, onCancel, onSubmit, children, submitTitle }: PopupFormProps) => {
+  const handleCancel = () => {
+    onCancel()
+  }
+
+  const handleSubmit = (event: Event) => {
+    event.preventDefault()
+    onSubmit()
+  }
+
+  return (
+    <div className={styles.veil}>
+      <div className={styles.popup}>
+        <h2 className={styles.title}>{title}</h2>
+        <div className={styles.closeButton}>
+          <Button icon={CloseIcon} onClick={handleCancel} appearance="flat" />
+        </div>
+        <form onSubmit={handleSubmit} className={styles.form}>
+          <div className={styles.formContent}>{children}</div>
+          <div className={styles.formActions}>
+            <Button type="submit" appearance="primary" title={submitTitle} />
+            <Button onClick={handleCancel} appearance="secondary" title="Cancel" />
+          </div>
+        </form>
+      </div>
+    </div>
+  )
+}
