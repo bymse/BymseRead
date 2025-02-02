7b6e66fc8dedeb58c23b60ac5dd1ddf92a56a83c
bymse <egorstrelov@gmail.com>
2025-02-02 11:14:31 +0300

    Basic routing setup

--- src/WebClientApp/src/app.tsx
+++ src/WebClientApp/src/app.tsx
@@ -1,40 +1,26 @@
-import { useState } from 'preact/hooks'
-import preactLogo from './assets/preact.svg'
-import viteLogo from '/vite.svg'
-import './app.css'
+import { ErrorBoundary, LocationProvider, Router, Route, useLocation } from 'preact-iso'
+import { BooksList } from './pages/BooksList/BooksList.tsx'
+import { useEffect } from 'preact/hooks'
 
 export function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://preactjs.com" target="_blank" rel="noreferrer">
-          <img src={preactLogo} className="logo preact" alt="Preact logo" />
-        </a>
-      </div>
-      <h1>Vite + Preact</h1>
-      <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/app.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p>
-        Check out{' '}
-        <a
-          href="https://preactjs.com/guide/v10/getting-started#create-a-vite-powered-preact-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          create-preact
-        </a>
-        , the official Preact + Vite starter
-      </p>
-      <p className="read-the-docs">Click on the Vite and Preact logos to learn more</p>
-    </>
+    <LocationProvider>
+      <ErrorBoundary>
+        <Router>
+          <Home path="/" />
+          <Route path="/books" component={BooksList} />
+        </Router>
+      </ErrorBoundary>
+    </LocationProvider>
   )
+}
+
+const Home = () => {
+  const { route } = useLocation()
+
+  useEffect(() => {
+    route('/books')
+  }, [])
+
+  return <></>
 }
---
+++ src/WebClientApp/src/pages/BooksList/BooksList.tsx
@@ -0,0 +1,3 @@
+﻿export const BooksList = () => {
+  return <div />
+}
