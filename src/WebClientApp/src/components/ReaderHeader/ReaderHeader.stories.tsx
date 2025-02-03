4aaaee8aac0822ba253b6a266539ee25a1eb249b
bymse <egorstrelov@gmail.com>
2025-02-03 09:11:43 +0300

    Reader header without number input

---
+++ src/WebClientApp/src/components/ReaderHeader/ReaderHeader.stories.tsx
@@ -0,0 +1,28 @@
+﻿import { ReaderHeader, ReaderHeaderProps } from './ReaderHeader.tsx'
+import { Meta, StoryObj } from '@storybook/preact'
+import { fn } from '@storybook/test'
+
+const WrappedHeader = (props: ReaderHeaderProps) => (
+  <div style={{ border: '1px dotted lightgray' }}>
+    <ReaderHeader {...props} />
+  </div>
+)
+
+const meta: Meta<ReaderHeaderProps> = {
+  component: WrappedHeader,
+  args: {},
+}
+export default meta
+
+type Story = StoryObj<ReaderHeaderProps>
+
+export const Default: Story = {
+  args: {
+    title: 'Long book title long book title long book title long book title long book title',
+    currentPage: 10,
+    totalPages: 100,
+    onBookmarkClick: fn(),
+    onEditBook: fn(),
+    onDeleteBook: fn(),
+  },
+}
---
+++ src/WebClientApp/src/components/ReaderHeader/ReaderHeader.tsx
@@ -0,0 +1,47 @@
+﻿import styles from './ReaderHeader.module.scss'
+import { Button } from '@components/Button/Button.tsx'
+import { ArrowLeftIcon } from '@icons/ArrowLeftIcon.tsx'
+import { Dropdown, DropdownItem } from '@components/Dropdown/Dropdown.tsx'
+import { BookmarkIcon } from '@icons/BookmarkIcon.tsx'
+import { Input } from '@components/Input/Input.tsx'
+
+export type ReaderHeaderProps = {
+  totalPages?: number
+  currentPage?: number
+  title: string
+  onBookmarkClick: () => void
+  onEditBook: () => void
+  onDeleteBook: () => void
+  onCurrentPageChange: (page: number) => void
+}
+
+export const ReaderHeader = ({
+  title,
+  onBookmarkClick,
+  onEditBook,
+  onDeleteBook,
+  totalPages,
+  currentPage,
+  onCurrentPageChange,
+}: ReaderHeaderProps) => {
+  const handleCurrentPageBlur = (value: string) => {
+    onCurrentPageChange(parseInt(value, 10))
+  }
+
+  return (
+    <header className={styles.header}>
+      <div className={styles.left}>
+        <Button href="/books" appearance="flat" icon={ArrowLeftIcon} />
+        <span className={styles.title}>{title}</span>
+      </div>
+      <div className={styles.center}></div>
+      <div className={styles.right}>
+        <Button icon={BookmarkIcon} appearance="flat" onClick={onBookmarkClick} />
+        <Dropdown side="left">
+          <DropdownItem onClick={onEditBook}>Edit book</DropdownItem>
+          <DropdownItem onClick={onDeleteBook}>Delete</DropdownItem>
+        </Dropdown>
+      </div>
+    </header>
+  )
+}
