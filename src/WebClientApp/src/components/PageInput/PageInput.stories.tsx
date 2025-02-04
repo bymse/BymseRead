7edd21e044769b6cf9bb8e5b30e4d3017a1b61b0
bymse <egorstrelov@gmail.com>
2025-02-04 09:30:51 +0300

    Page input with resize

---
+++ src/WebClientApp/src/components/PageInput/PageInput.stories.tsx
@@ -0,0 +1,25 @@
+﻿import { PageInput, PageInputProps } from './PageInput.tsx'
+import { Meta, StoryObj } from '@storybook/preact'
+import { fn } from '@storybook/test'
+
+const WrappedInput = (props: PageInputProps) => (
+  <span style={{ border: '1px dotted lightgray', padding: '10px' }}>
+    <PageInput {...props} />
+  </span>
+)
+
+const meta: Meta<PageInputProps> = {
+  component: WrappedInput,
+  args: {},
+}
+export default meta
+
+type Story = StoryObj<PageInputProps>
+
+export const Default: Story = {
+  args: {
+    defaultValue: 10,
+    maxNumber: 100,
+    onBlur: fn(),
+  },
+}
---
+++ src/WebClientApp/src/components/PageInput/PageInput.tsx
@@ -0,0 +1,62 @@
+﻿import { useState, useEffect, useRef } from 'react'
+import styles from './PageInput.module.css'
+
+export type PageInputProps = {
+  defaultValue?: number
+  maxNumber?: number
+  onValueChange?: (value: number) => void
+}
+
+export const PageInput = ({ defaultValue, maxNumber, onValueChange }: PageInputProps) => {
+  const [inputWidth, setInputWidth] = useState('auto')
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  const updateWidth = () => {
+    if (inputRef.current) {
+      const length = (inputRef.current.value || defaultValue?.toString() || '').length
+      const width = length === 0 ? 1 : length > 5 ? 5 : length
+      setInputWidth(`${width}ch`)
+    }
+  }
+
+  const updateValue = (event: Event) => {
+    const inputValue = parseInt((event.target as HTMLInputElement).value, 10)
+    const value = isNaN(inputValue) ? 1 : maxNumber && inputValue > maxNumber ? maxNumber : inputValue
+    if (inputRef.current) {
+      inputRef.current.value = value.toString()
+      updateWidth()
+    }
+    if (onValueChange) {
+      onValueChange(value)
+    }
+  }
+
+  const handleBlur = (event: FocusEvent) => {
+    updateValue(event)
+  }
+
+  const handleKeyDown = (event: KeyboardEvent) => {
+    if (event.key === 'Enter') {
+      updateValue(event)
+    }
+  }
+
+  useEffect(() => {
+    updateWidth()
+  }, [defaultValue])
+
+  return (
+    <input
+      ref={inputRef}
+      className={styles.input}
+      type="number"
+      defaultValue={defaultValue}
+      onBlur={handleBlur}
+      onKeyDown={handleKeyDown}
+      max={maxNumber}
+      onInput={updateWidth}
+      style={{ width: inputWidth }}
+      placeholder={defaultValue?.toString()}
+    />
+  )
+}
