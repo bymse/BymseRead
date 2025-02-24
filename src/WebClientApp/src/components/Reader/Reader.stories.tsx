9c53c850cf1a0cdcb996b351977aa9961a884bdd
bymse <egorstrelov@gmail.com>
2025-02-24 22:27:06 +0300

    Initial pdf reader

---
+++ src/WebClientApp/src/components/Reader/Reader.stories.tsx
@@ -0,0 +1,15 @@
+﻿import { Reader, ReaderProps } from './Reader.tsx'
+import { Meta, StoryObj } from '@storybook/preact'
+
+const meta: Meta<ReaderProps> = {
+  component: Reader,
+}
+export default meta
+
+type Story = StoryObj<ReaderProps>
+
+export const Default: Story = {
+  args: {
+    pdfUrl: 'example.pdf',
+  },
+}
---
+++ src/WebClientApp/src/components/Reader/Reader.tsx
@@ -0,0 +1,61 @@
+﻿import { useEffect, useRef } from 'preact/hooks'
+import * as pdfjsLib from 'pdfjs-dist'
+import { PDFViewer, EventBus } from 'pdfjs-dist/web/pdf_viewer.mjs'
+import styles from './Reader.module.scss'
+
+pdfjsLib.GlobalWorkerOptions.workerSrc = new URL('pdfjs-dist/build/pdf.worker.min.mjs', import.meta.url).toString()
+
+export type ReaderProps = {
+  pdfUrl: string
+  currentPage?: number
+  onCurrentPageChange?: (page: number) => void
+}
+
+export const Reader = ({ pdfUrl, currentPage, onCurrentPageChange }: ReaderProps) => {
+  const containerRef = useRef<HTMLDivElement>(null)
+  const viewerRef = useRef<PDFViewer | null>(null)
+  const lastPageRef = useRef<number | null>(null)
+
+  useEffect(() => {
+    if (!containerRef.current) return
+
+    const eventBus = new EventBus()
+    eventBus.on('pagechanging', ({ pageNumber }: { pageNumber: number }) => {
+      if (pageNumber !== lastPageRef.current) {
+        if (onCurrentPageChange) {
+          onCurrentPageChange(pageNumber)
+        }
+        lastPageRef.current = pageNumber
+      }
+    })
+    const pdfViewer = new PDFViewer({
+      container: containerRef.current,
+      eventBus,
+      removePageBorders: true,
+    })
+    viewerRef.current = pdfViewer
+
+    void pdfjsLib.getDocument(pdfUrl).promise.then(pdfDocument => {
+      pdfViewer.setDocument(pdfDocument)
+      if (currentPage) {
+        pdfViewer.currentPageNumber = currentPage
+      }
+    })
+  }, [pdfUrl])
+
+  useEffect(() => {
+    if (viewerRef.current && currentPage) {
+      viewerRef.current.currentPageNumber = currentPage
+    }
+  }, [currentPage])
+
+  return (
+    <section className={styles.root}>
+      <div className={styles.wrapper}>
+        <div className={styles.pdfjsContainer} ref={containerRef}>
+          <div id="pdf-viewer" className="pdfViewer"></div>
+        </div>
+      </div>
+    </section>
+  )
+}
