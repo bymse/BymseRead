301f25114172a821bb0268dd50d61ee775d8fc11
bymse <egorstrelov@gmail.com>
2025-04-20 22:25:51 +0300

    Delete legacy

--- src/BymseRead.Legacy.Core/Models/BookmarksListModel.cs
+++
@@ -1,7 +0,0 @@
-namespace BymseRead.Legacy.Core.Models;
-
-public class BookmarksListModel
-{
-    public BookmarkModel[] CustomBookmarks { get; init; }
-    public BookmarkModel? LastPageBookmark { get; init; }
-}
