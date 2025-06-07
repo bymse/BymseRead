9af21368b057b83a2e0f9a110be91dea1f33f06b
bymse <egorstrelov@gmail.com>
2025-06-07 22:09:02 +0300

    Fix hovers

--- src/WebClientApp/src/components/BookCard/BookCard.tsx
+++ src/WebClientApp/src/components/BookCard/BookCard.tsx
@@ -1,5 +1,5 @@
 ﻿import styles from './BookCard.module.scss'
-import { useState } from 'preact/hooks'
+import {useState} from 'preact/hooks'
 
 export type BookCardProps = {
   title: string
@@ -8,30 +8,32 @@ export type BookCardProps = {
   bookId: string
 }
 
-export const BookCard = ({ title, coverUrl, percentageFinished, bookId }: BookCardProps) => {
+export const BookCard = ({title, coverUrl, percentageFinished, bookId}: BookCardProps) => {
   const [imageLoaded, setImageLoaded] = useState(false)
 
   return (
-    <a style={{ ['--fill-percent']: `${percentageFinished || 0}%` }} className={styles.card} href={`/books/${bookId}`}>
-      {coverUrl && (
-        <>
-          <img
-            className={styles.cover}
-            src={coverUrl}
-            alt={title}
-            style={{ display: imageLoaded ? 'block' : 'none' }}
-            onLoad={() => setImageLoaded(true)}
-          />
-          {!imageLoaded && <DefaultBookCover title={title} />}
-        </>
-      )}
-      {!coverUrl && <DefaultBookCover title={title} />}
-      <div className={styles.title}>{title}</div>
-    </a>
+    <div className={styles.cardWrapper}>
+      <a style={{['--fill-percent']: `${percentageFinished || 0}%`}} className={styles.card} href={`/books/${bookId}`}>
+        {coverUrl && (
+          <>
+            <img
+              className={styles.cover}
+              src={coverUrl}
+              alt={title}
+              style={{display: imageLoaded ? 'block' : 'none'}}
+              onLoad={() => setImageLoaded(true)}
+            />
+            {!imageLoaded && <DefaultBookCover title={title}/>}
+          </>
+        )}
+        {!coverUrl && <DefaultBookCover title={title}/>}
+        <div className={styles.title}>{title}</div>
+      </a>
+    </div>
   )
 }
 
-const DefaultBookCover = ({ title }: { title: string }) => {
+const DefaultBookCover = ({title}: { title: string }) => {
   return (
     <div className={styles.defaultCover}>
       <span className={styles.coverLetter}>{title[0]}</span>
