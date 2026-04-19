70fee5f582380a71b2c32e00cb86becd5659acdd
Egor Strelov <egorstrelov@gmail.com>
2026-04-19 21:40:19 +0300

    Mobile bookmarks width

--- src/WebClientApp/src/components/SidePanel/SidePanel.tsx
+++ src/WebClientApp/src/components/SidePanel/SidePanel.tsx
@@ -2,6 +2,7 @@ import { ComponentChildren } from 'preact'
 import { Button } from '@components/Button/Button.tsx'
 import { RemoveIcon } from '@icons/RemoveIcon.tsx'
 import styles from './SidePanel.module.scss'
+import cn from 'classnames'
 
 export type SidePanelProps = {
   title: ComponentChildren
@@ -9,11 +10,12 @@ export type SidePanelProps = {
   children: ComponentChildren
   testId?: string
   closeButtonTestId?: string
+  className?: string
 }
 
-export const SidePanel = ({ title, onClose, children, testId, closeButtonTestId }: SidePanelProps) => {
+export const SidePanel = ({ title, onClose, children, testId, closeButtonTestId, className }: SidePanelProps) => {
   return (
-    <div className={styles.container} data-testid={testId}>
+    <div className={cn(styles.container, className)} data-testid={testId}>
       <div className={styles.header}>
         <h2 className={styles.title}>{title}</h2>
         <div className={styles.close}>
